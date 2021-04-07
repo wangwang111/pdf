6f835e322926d07d39af7ed9aa7daaf5fdda3da2
wangshaoshuai <wangshaoshuai>
2021-04-07 09:57:58 +0800

    tlab参数

--- ShanZhanJvm/src/main/java/chapter5/UseTLAB.java
+++ ShanZhanJvm/src/main/java/chapter5/UseTLAB.java
@@ -3,6 +3,12 @@ package chapter5;
 /**
  * @author WangShaoShuai
  * @date 2021/03/29
+ *
+ * 在tlab上分配对象
+ * -XX:+UseTLAB -Xcomp -XX:-BackgroundCompilation -XX:-DoEscapeAnalysis
+ *
+ * 禁用tlab
+ * -XX:+UseTLAB -Xcomp -XX:-BackgroundCompilation -XX:-DoEscapeAnalysis
  */
 public class UseTLAB {
 
@@ -13,7 +19,7 @@ public class UseTLAB {
 
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
-        for (int i = 0; i < 1000000; i++) {
+        for (int i = 0; i < 10000000; i++) {
             alloc();
         }
         long end = System.currentTimeMillis();
